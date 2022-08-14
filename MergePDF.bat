8907d72c688c4d8bad196a07215e4f06cfccc035
Nikita Patil <patilnikita296@gmail.com>
2022-08-14 14:24:21 +0530

    Bat file

--- MergePDF.bat
+++ MergePDF.bat
@@ -1,4 +1,10 @@
 @echo off
+
+echo ----------------------------------------------------------------------------
+echo                             Author: Nikita Patil
+echo                             Date  : 14/08/2022
+echo ----------------------------------------------------------------------------
 "C:\Users\NIKITA\AppData\Local\Programs\Python\Python38\python.exe" "D:\STUDY\PYTHON\MergePDF\MergePdf.py" 
+echo Pdf files Merged.......
 pause
 
