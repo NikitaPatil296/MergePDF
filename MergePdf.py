66bd7689d9fc8cfbade472f61711ebd89688f12a
Nikita Patil <patilnikita296@gmail.com>
2022-08-14 12:50:15 +0530

    Merged two pdf files

---
+++ MergePdf.py
@@ -0,0 +1,15 @@
+from PyPDF2 import PdfFileMerger
+
+#Create and instance of PdfFileMerger() class
+merger = PdfFileMerger()
+
+#Create a list with file names
+pdf_files = ['pdf_files/my documents.pdf', 'pdf_files/my document 1.pdf']
+
+for pdf_file in pdf_files:
+    #Append PDF files
+    merger.append(pdf_file)
+
+#Write out the merged PDF
+merger.write("merged_2_pages.pdf")
+merger.close()
